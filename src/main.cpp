326b84ff9c3b9491d5ae48ef45a0636c5f645cf0
Gabriel Menino <grmenino@gmail.com>
2025-10-03 18:22:25 -0300

    First declaration test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
 #include <Arduino.h>
 #include <enum/Types.h>
+#include <structs/Pokemon.h>
 
-Type type = FIRE;
+Pokemon pokemon;
 
 void setup() {
+  pokemon.name = "Bulbasaur";
+  pokemon.id = 1;
+  pokemon.level = 1;
+
   Serial.begin(9600);
 }
 
 void loop() {
-  if(type == FIRE) {
-    Serial.println("Fogo");
-  }
+  Serial.print("Name: ");
+  Serial.println(pokemon.name);
+  Serial.print("Number: ");
+  Serial.println(pokemon.id);
+  Serial.print("Level: ");
+  Serial.println(pokemon.level);
+
+  delay(99999);
 }
