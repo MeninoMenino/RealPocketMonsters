62cb6b583e91c28fa0e5c0e595518b5a484ab282
Gabriel Menino <grmenino@gmail.com>
2025-10-12 23:08:54 -0300

    Type to string function

--- include/enum/Type.h
+++ include/enum/Type.h
@@ -26,4 +26,30 @@ typedef enum {
   NONE_TYPE
 } Type;
 
+const char* typeToString(Type type) {
+  switch (type) {
+    case NORMAL_TYPE: return "NORMAL";
+    case FIRE_TYPE: return "FIRE";
+    case WATER_TYPE: return "WATER";
+    case ELECTRIC_TYPE: return "ELECTRIC";
+    case GRASS_TYPE: return "GRASS";
+    case ICE_TYPE: return "ICE";
+    case FIGHTING_TYPE: return "FIGHTING";
+    case POISON_TYPE: return "POISON";
+    case GROUND_TYPE: return "GROUND";
+    case FLYING_TYPE: return "FLYING";
+    case PSYCHIC_TYPE: return "PSYCHIC";
+    case BUG_TYPE: return "BUG";
+    case ROCK_TYPE: return "ROCK";
+    case GHOST_TYPE: return "GHOST";
+    case DRAGON_TYPE: return "DRAGON";
+    case DARK_TYPE: return "DARK";
+    case STEEL_TYPE: return "STEEL";
+
+    case CURSE_TYPE: return "CURSE";
+    case NONE_TYPE: return "NONE";
+    default: return "UNKNOWN";
+  }
+}
+
 #endif
